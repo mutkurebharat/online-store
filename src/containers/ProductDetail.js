ba09ad2ef89a24134345a18487ab7df1f3330d69
mutkurebharat <34864760+mutkurebharat@users.noreply.github.com>
2022-02-10 12:03:17 +0530

    Update ProductDetail.js

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+//import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -45,12 +45,12 @@ const ProductDetail = () => {
             <div className="ui vertical divider">AND</div>
             <div className="middle aligned row">
               <div className="column lp">
-                <img className="ui fluid image" src={image} />
+                <img className="ui fluid image" alt={title} src={image} />
               </div>
               <div className="column rp">
                 <h1>{title}</h1>
                 <h2>
-                  <a className="ui teal tag label">${price}</a>
+                  <a className="ui teal tag label" href="#">${price}</a>
                 </h2>
                 <h3 className="ui brown block header">{category}</h3>
                 <p>{description}</p>
